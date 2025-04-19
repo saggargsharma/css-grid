d5632c21a48f02b8ae8de0a8d9710b156e54f14e
Saggar G Sharma <hello@saggargsharma.me>
2025-04-19 14:42:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var nav = document.getElementById('nav');
+
+document.getElementById('nav-open').addEventListener('click', function() {
+  nav.style.display = 'block';
+});
+
+document.getElementById('nav-close').addEventListener('click', function() {
+  nav.style.display = 'none';
+});
